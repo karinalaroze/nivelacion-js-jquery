ecddec058c95b18d71694d878c569d1e2dfffcd7
KarinaLaroze <karinalaroze@gmail.com>
2017-07-10 14:10:35 -0400

    Etapa 5: cree una funcion renderActivitys agregando un if con un hide

--- js/script-index.js
+++ js/script-index.js
@@ -11,6 +11,8 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
+	renderActivities(activitiesArray);
+
 
 
 });
@@ -62,6 +64,12 @@ function renderRecipe(recipe) {
 */
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
+	activitiesArray.forEach(function(elem){
+		renderActivity(elem);
+	})
+	if(activitiesArray.length > 0){
+		$('.wrapper-message').hide();
+	}
 }
 
 /*
