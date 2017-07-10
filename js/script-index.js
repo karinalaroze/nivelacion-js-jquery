6ba4ece61e99c3053b6e626cfc9440c90cc155ed
KarinaLaroze <karinalaroze@gmail.com>
2017-07-10 13:44:21 -0400

    Etapa 4: agregue en el script index un append con los span y realize la ruta para que se me vieran las imagenes con su nombre

--- js/script-index.js
+++ js/script-index.js
@@ -38,7 +38,22 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+	$(".list-recipes").append(`<a class="item-recipe" href="#">
+  <span class="attribution">
+    <span class="title-recipe"> `+ recipe.title +`</span>
+    <span class="metadata-recipe">
+      <span class="author-recipe"> `+ recipe.source.name +` </span>
+      <span class="bookmarks-recipe">
+        <span class="icon-bookmark"></span> 
+      </span>
+    </span>
+  </span>
+
+  <img src="img/recipes/320x350/`+ recipe.name + `.jpg" />
+</a>
+`)
 }
+
 
 
 
